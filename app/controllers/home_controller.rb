19de71fa3c0b1d92488f2212032fbf978faafbbf
daichi <daichi@suzukidaichinoMacBook-Pro-2.local>
2020-01-26 20:38:53 +0900

    first

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,7 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def boot
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#top'
   get 'home/about' => "home#about"
+  get 'home/boot' => "home#boot"
 
   devise_for :users
 
